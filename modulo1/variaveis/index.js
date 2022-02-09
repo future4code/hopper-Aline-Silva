8cbbcc355d3fb720a777443111f7bd614c4bee5b
MariLika <aline.mari1986@gmail.com>
2022-02-08 23:58:38 -0300

    exercicios

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,59 @@
+/* Resposta do exercício de interpretação 1
+ console.log(a, b) = 10, 5 
+
+ Resposta do exercicío de interpretação 2
+ consolo.log(a, b, c) = 10, 10, 10
+
+ Resposta do exercício de interpretação 3 
+  let horaTrabalho = prompt("Quantas horas você trabalha por dia?")
+  let valorDia = prompt("Quanto você recebe por dia?")
+  alert(`Voce recebe ${horaTrabalho/valorDia} por hora`) */
+  
+  
+ /*let nome 
+ let idade
+ console.log(typeof nome , typeof idade)*/
+
+ /* O typeof deu não identificado por que não foi
+ atribuido nenhum valor*/
+
+ /*let nome = prompt("Digite seu nome")
+ let idade = prompt("Digite sua idade")
+ 
+ console.log(typeof nome , typeof idade)
+
+  Notei que o typeof foi identificado, pois 
+ foi adicionado o valor de nome e idade */
+
+// alert(`Olá ${nome} você tem ${idade} anos`)//
+
+
+
+let respostaPergunta1 = prompt("Você se alimentou hoje?  ")
+let respostaPergunta2 = prompt("Você foi a aula hoje?  ")
+let respostaPergunta3 = prompt("Você viu a minha bolsa?  ")
+
+console.log("Você se alimentou hoje?",respostaPergunta1)
+("Você viu a minha bolsa?", respostaPergunta3)
+console.log
+
+let a = 10
+let b = 25
+
+a = b
+b = a
+
+
+
+
+
+
+
+  
+
+ 
+ 
+
+
+
+
