73db2ae6a55310ce72d928f549cd097fff11fee4
MariLika <aline.mari1986@gmail.com>
2022-02-10 23:55:12 -0300

    operadores a terminar

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,20 @@
+/*Exercício de inter pretação de codigo 
+1.
+Result. a = false
+Result. b = false
+Result. c = true
+Result. d = Boolean
+
+2.
+O erro se deu por que sou  foi atribuido o 
+Number antes de prompt, então ñ foi feito uma soma e sim 
+foi concatendo os doid números
+
+3. 
+É necessário colocar o Number antes de prompt */
+
+let numero = prompt("Digite sua idade")
+console.log
+
+
+
