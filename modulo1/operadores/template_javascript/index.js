8a75772d5e0be55f9bd932fe50372776db500541
MariLika <aline.mari1986@gmail.com>
2022-02-15 21:18:07 -0300

    Exercicío finalizado

--- modulo1/operadores/template_javascript/index.js
+++ modulo1/operadores/template_javascript/index.js
@@ -13,8 +13,41 @@ foi concatendo os doid números
 3. 
 É necessário colocar o Number antes de prompt */
 
-let numero = prompt("Digite sua idade")
-console.log
+
+// Exercício de escrita de código//
+
+//1.//
+
+/*let idade1 = Number (prompt ("Digite sua idade"))
+let idade2 = Number (prompt ("Digite a idade do seu melhor amigo ou amiga"))
+let idade = idade1 > idade2
+
+console.log("Sua idade é maior que a do seu amigo", idade)*/  
 
 
+//2.//
 
+let resultado = Number (prompt("Digite um numero par"))
+
+console.log (resultado % 2)
+
+// Notei que qual numero par da o resto da divisão "0"//
+
+// Quando colocamos numeros ímpares o resto da divisão sempre da "1"//
+
+//3//
+
+let idade 
+
+
+//4//
+
+
+let num1
+let num2 
+let numero= Number = prompt("Coloque dois numeros")
+
+console.log(num1 > num2)
+console.log(num1 === num2)
+console.log(num1 / num2) 
+console.log(num2 / num1) 
