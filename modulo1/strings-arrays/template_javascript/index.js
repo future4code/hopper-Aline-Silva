0a0f67eaea054cde54063478626d3af017b25a68
MariLika <aline.mari1986@gmail.com>
2022-02-15 23:37:58 -0300

    Exercício Arrays a terminar

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,21 @@
+// Exercícios de interpretação//
+
+//1°//
+
+//resultado 1- undefined//
+//resultado 2- null//
+//resultado 3- 11//
+//resultado 4- 3//
+//resultado 5- array(11)//
+//resultado 6- 9//
+
+//2°//
+
+/* Todas as letras ficaram em caixa alta, me dira a quantidade de caracteres 
+que são 27*/
+const nome = prompt("Escreva seu nome") 
+const emailDoUsuario = prompt("Digite seu e-mail")
+
+console.log("o ("emailDoUsuario") foi cadastrado com sucesso. Seja bem vinda(o) ("nome") ")
+console.log(email.trim())
+ 
