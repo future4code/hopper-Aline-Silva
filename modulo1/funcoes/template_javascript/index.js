ee5b98c1d63d2d96f64eab787f6a8d37a41620cf
MariLika <aline.mari1986@gmail.com>
2022-02-17 23:30:59 -0300

    Execício funções a terminar

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,27 @@
+//-------Execício de interpretação-------//
+
+//1.
+
+//a
+//10, 50
+
+//b 
+//nada
+
+
+//2.
+
+//a
+//A função esta pedindo um texto que contenha "cenoura" 
+//e identificar se é false ou true
+
+//b 
+//I =   true
+//II =  true
+//III = true
+
+
+
+//--------Exercícios de escrita  de codigo--------//
+
+//1.
