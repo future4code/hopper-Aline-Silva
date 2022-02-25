57529b33d8d1a4d00a50c101babd1c73bf40a262
MariLika <aline.mari1986@gmail.com>
2022-02-24 23:16:06 -0300

    condicionais a terminar

---
+++ modulo1/Condicionais/index.js
@@ -0,0 +1,106 @@
+//-----------Exercício de interpretação de texto-----------//
+
+//1
+
+//a= O numero que o usuario utilizar sera feito mudulo que verifica se o
+// resultado sera par ou impar
+
+//b= números pares
+
+//c= números impares
+
+
+
+
+//2
+
+//a= Serve para fazer um condicionamento de forma simplificada
+
+//b= O preço da fruta Maçã é de R$ 5 ao invez de 2,25
+
+//c= Mostrara o preço da fruta Pêra é de R$ 5 ao invez  de 5,5
+
+
+
+//3
+
+//a= Pede ao usuário que digite um número
+
+//b= No console estará que o 10 é maior q 0, e for por -10 dira q não   
+
+//c= sim vai informar q a mensagem não foi declarada, acredito que cm o Let esta
+// fora fora do lugar
+ 
+
+
+//-----------Exercício de escrita de código-----------//
+
+//1.
+
+/*const idadeUsuario = Number (prompt("Digite sua idade")) 
+
+function verificaIdade(idade){
+
+    if (idade >= 18){                         
+      console.log("Ele/Ela pode dirigir")
+     }
+    else {
+      console.log("Você não pode dirigir") 
+     }
+}
+verificaIdade(idadeUsuario)*/
+
+//2.
+
+/*const turnoMatutino = prompt("Digite `M` se você estuda no matutino")
+const turnoVespertino = prompt("Digite `V` se você estuda no vespertino")
+const turnoNoturno = prompt("Digite `N` se você estuda no noturno")
+
+function verificaTurno(turnoMatutino, turnoVespertino, turnoNoturno){
+
+    if (turnoMatutino === M){
+        console.log("Bom Dia")
+    }
+    else if (turnoVespertino === V){
+        console.log("Boa tarde")
+    }
+    else if (turnoNoturno === N){
+        console.log("Boa noite")
+
+    }    
+}
+
+verificaTurno(turnoMatutino, turnoVespertino, turnoNoturno)*/
+
+//3.
+
+const Turno = prompt("Digite seu Turno")
+
+switch (Turno){
+
+    case `M`:
+        console.log("Bom dia")
+    break
+ 
+    case `V`:
+        console.log("Boa tarde")
+    break    
+    
+    case `N`:
+        console.log("Boa noite")
+    break
+    
+    default :
+        console.log("Algo saiu errado, tente novamente")
+    break    
+    
+}    
+
+
+//4.
+
+const generoFilme = prompt("digite o genero do filme")
+const valorIngressso = prompt("Coloque o valor do ingresso")
+
+
+
