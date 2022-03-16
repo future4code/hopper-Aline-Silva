efe6486abff6f3d1934bfe39c1566fd6c7f15f49
MariLika <aline.mari1986@gmail.com>
2022-03-15 23:56:11 -0300

    resolvendo-problemas

---
+++ modulo1/resolvendo-problemas/index.js
@@ -0,0 +1,15 @@
+// ----------Ecercícios de fixação de conhecimento----------
+
+/* 
+1. Ler o enuncioado com atenção
+2. Separar palavras chaves 
+3. Como e quais serão os dados recebidos
+4. Criar exemplos de situações antes
+5. colocar no papel todas as possíveis soluções
+6. solluçoes obvias e pouco performáticas
+7 dar bomns nomes para as variáveis e funções*/
+
+
+/* Eu vejo esses 7 passos como um norte, pois eu ficava 
+bem perdida por onde começar, quando olhava para um enunciado, e esses 7 passos
+foram fundamentais para mim*/
